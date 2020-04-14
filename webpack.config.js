6a5b4403326447ace6f504338782c54ca9cc12ce
Ryan Cain <racain05@gmail.com>
2020-04-14 16:48:38 -0400

    installed Html-webpack-plugin dev-dependency and modified file structure for bundle build

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: [
     './src/index.js',
   ],
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
   },
   module: {
     rules: [
       {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Homepage',
+      template: 'index.html'
+    }
+    )
+  ]
 }
